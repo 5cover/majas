16d5b39e1c4e4b4e020c8e10e444447781b25132
Scover <>
2025-08-08 12:19:37 +0200

    api

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-// core api declaration here
+// majas API: type-aware, format-agnostic conversion
+
+import type IRNode from './core/IRNode.js';
+import type Document from './core/Document.js';
+import type { Format, Options, OptionsSchema } from './core/Format.js';
+
+import formats from './core/formats.js';
+
+export const Formats = formats;
+
+export { inferFormat, findFormat } from './util/misc.js';
+
+export type { IRNode, Document, Format, Options, OptionsSchema };
